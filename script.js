5023baa49969c2960beaceb21c5609da12593889
Lucas Rocha <lucassanrocha@gmail.com>
2023-03-08 18:50:44 -0300

    update 1

--- script.js
+++ script.js
@@ -2,12 +2,10 @@
 
 function calcularValores(){
     // input 
-
     const valor = [ document.getElementById ('dxFechamento'),document.getElementById('dxAbertura'),document.getElementById('dxMinima'),document.getElementById('dxMaxima'),document.getElementById('dolarFechamento'),document.getElementById('dolarAjuste')
     ]
     const valoresNumber = [Number (valor[0].value),Number (valor[1].value), Number (valor[2].value),Number (valor[3].value),Number (valor[4].value),Number (valor[5].value)
     ] 
-
     /* 
     valoresNumber[0] Fechamento do Dx
     valoresNumber[1] Abertura do DX
@@ -16,19 +14,10 @@ function calcularValores(){
     valoresNumber[4] Fechamento Dolar
     valoresNumber[5] Ajuste Dolar
     */
-
     function varPercentual (a,b){ return (((b-a)/a)*100)}
     function maxVariacao (a){ return ((a*(0,5/100)/10)+a)} 
     function minVariacao (a){ return ((a*(0,5/100)/10)-a)} 
-   
-   
-   
-    function resultadoDeVariacao(a,b){  
-        return (b*(a/100)+b) 
-    }
-
-
-
+    function resultadoDeVariacao(a,b){return (b*(a/100)+b) }
     let quadroDx = [
         varPercentual(valoresNumber[0],valoresNumber[1]),varPercentual(valoresNumber[0],valoresNumber[2]),varPercentual(valoresNumber[0],valoresNumber[3])
 ]
@@ -39,7 +28,6 @@ function calcularValores(){
 */
 let abertuta = resultadoDeVariacao(quadroDx[0],valoresNumber[4])
 
-    
     resdx;dx.innerHTML = `Max Abertura :${ maxVariacao(abertuta).toPrecision(6)}`
     resdx;dx.innerHTML += '<br>'
     resdx;dx.innerHTML += `Abertura :${ abertuta.toPrecision(6)}`
@@ -50,7 +38,6 @@ let abertuta = resultadoDeVariacao(quadroDx[0],valoresNumber[4])
     resdx;dx.innerHTML += `Min Dx: ${resultadoDeVariacao(quadroDx[1],valoresNumber[4]).toPrecision(6)}`
     resdx;dx.innerHTML += '<br>'
     resdx;dx.innerHTML += `Max Dx: ${resultadoDeVariacao(quadroDx[2],valoresNumber[4]).toPrecision(6)}`
-    
     resab;ab.innerHTML = `Fechamento Dolar: ${valoresNumber[4]}`
     resab;ab.innerHTML += '<br>'
     resaj;aj.innerHTML = `Max Ajuste ${maxVariacao(valoresNumber[5]).toPrecision(6)}`
