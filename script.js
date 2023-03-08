a14d0dd422f2f5cedbf5a930100c49feba0a9ddb
Lucas Rocha <lucassanrocha@gmail.com>
2023-03-08 20:51:14 -0300

    update
    style novo

--- script.js
+++ script.js
@@ -28,24 +28,25 @@ function calcularValores(){
 */
 let abertuta = resultadoDeVariacao(quadroDx[0],valoresNumber[4])
 
-    resdx;dx.innerHTML = `Max Abertura :${ maxVariacao(abertuta).toPrecision(6)}`
+    resdx.innerHTML = `<p id="dx">Max Abertura :${ maxVariacao(abertuta).toPrecision(6)} </p>  `
     resdx;dx.innerHTML += '<br>'
     resdx;dx.innerHTML += `Abertura :${ abertuta.toPrecision(6)}`
     resdx;dx.innerHTML += '<br>'
     resdx;dx.innerHTML += `Min Abertura :${ minVariacao(abertuta*-1).toPrecision(6)}`
     resdx;dx.innerHTML += '<br>'
-    resdx;dx.innerHTML += '<br>'
     resdx;dx.innerHTML += `Min Dx: ${resultadoDeVariacao(quadroDx[1],valoresNumber[4]).toPrecision(6)}`
     resdx;dx.innerHTML += '<br>'
     resdx;dx.innerHTML += `Max Dx: ${resultadoDeVariacao(quadroDx[2],valoresNumber[4]).toPrecision(6)}`
-    resab;ab.innerHTML = `Fechamento Dolar: ${valoresNumber[4]}`
+   
+    resab.innerHTML = `<p id="ab">Fechamento Dolar: ${valoresNumber[4]}</p> `
     resab;ab.innerHTML += '<br>'
-    resaj;aj.innerHTML = `Max Ajuste ${maxVariacao(valoresNumber[5]).toPrecision(6)}`
+    
+    resaj.innerHTML = `<p id="aj">Max Ajuste ${maxVariacao(valoresNumber[5]).toPrecision(6)}</p> `
     resaj;aj.innerHTML += '<br>'
     resaj;aj.innerHTML += `Ajuste${valoresNumber[5]}`
     resaj;aj.innerHTML += '<br>'
     resaj;aj.innerHTML += `Min Ajuste ${minVariacao(valoresNumber[5]*-1).toPrecision(6)}`
-
+/* 
 console.log(resultadoDeVariacao(quadroDx[0],valoresNumber[4]).toPrecision(6))
 console.log(resultadoDeVariacao(quadroDx[1],valoresNumber[4]).toPrecision(6))
 console.log(resultadoDeVariacao(quadroDx[2],valoresNumber[4]).toPrecision(6))
@@ -53,6 +54,7 @@ console.log(valoresNumber[4])
 console.log(maxVariacao(valoresNumber[5]).toPrecision(6))
 console.log(valoresNumber[5])
 console.log(minVariacao(valoresNumber[5]).toPrecision(6))
+*/
 
 }
 
